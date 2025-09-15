4d0cc3c4ca20c32acc594780e96947ecce7bd707
Ajay Sharma <gridking111@gmail.com>
2025-09-15 17:02:51 +0300

    Initial Commit

---
+++ LuvDeck/LuvDeckApp.swift
@@ -0,0 +1,17 @@
+//
+//  LuvDeckApp.swift
+//  LuvDeck
+//
+//  Created by Work Laptop on 15/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct LuvDeckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LuvDeckTests/LuvDeckTests.swift
@@ -0,0 +1,17 @@
+//
+//  LuvDeckTests.swift
+//  LuvDeckTests
+//
+//  Created by Work Laptop on 15/09/2025.
+//
+
+import Testing
+@testable import LuvDeck
+
+struct LuvDeckTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
